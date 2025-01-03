8a4edbe07b47f64df333db2e0466700c50e901c9
dru <ahsieh@wealthsimple.com>
2025-01-02 21:24:44 -0500

    Change to typescript, and correctly render the rice dude

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import RicePlaceholder from "./components/RicePlaceholder"
 
 function App() {
   return (
     <div className="App">
-      
+      <RicePlaceholder />
     </div>
   );
 }
---
+++ src/components/RicePlaceholder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "../assets/rice_placeholder.png"
+
+const RicePlaceholder = () => {
+  return (
+    <>
+      <img src={Image} alt="Rice Placeholder" />
+    </>
+  )
+};
+
+export default RicePlaceholder;
